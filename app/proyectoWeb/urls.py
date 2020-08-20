c2ac1613be86329bdea5c84ebce5c553af80a8b0
EmanuelX92 <emanuelX92@gmail.com>
2020-08-20 17:26:10 -0400

    comienzo pildoras informaticas

---
+++ app/proyectoWeb/urls.py
@@ -0,0 +1,6 @@
+#from django.urls import path
+#from proyectoweb.views import index
+#//TODO : aqui tengo que hacer las urls para cada app, investigar
+urlpatterns = [
+ #   path('/',index , name='index'),
+]
