97b4549096bb4d57db208f2c26a2eba45bf5b8d0
EmanuelX92 <emanuelX92@gmail.com>
2020-08-20 17:48:28 -0400

    agregar archivos .ignore

--- AppWeb/settings.py
+++ AppWeb/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'AppWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'pildoras',
+        'USER':'',
+        'PASSWORD':'ventex',
+        'HOST':'localhost',
+        'PORT':'',
     }
 }
 
